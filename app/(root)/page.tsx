998d632bb7ff58d0dbb3a7058473a4ed902bc88f
Jade <jade@adopstar.com>
2024-05-29 14:17:49 +0100

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,38 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Jade",
+    lastName: "Page",
+    email: "jade@adopstar.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions effeciently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+      />
+    </section>
+  );
 };
 export default Home;
