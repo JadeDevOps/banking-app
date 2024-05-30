071d2f7262f7560fbd08c2e36140cc53194a8a9f
Jade <jade@adopstar.com>
2024-05-30 13:56:46 +0100

    implement auth, appwrite and sentry

---
+++ components/CustomFormInput.tsx
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { FormControl, FormField, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+import { Control, FieldPath } from "react-hook-form";
+import { authFormSchema } from "@/lib/utils";
+
+const formSchema = authFormSchema("sign-up");
+
+interface CustomInput {
+  control: Control<z.infer<typeof formSchema>>;
+  name: FieldPath<z.infer<typeof formSchema>>;
+  label: string;
+  placeholder: string;
+}
+const CustomFormInput = ({
+  control,
+  name,
+  label,
+  placeholder,
+}: CustomInput) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel>{label}</FormLabel>
+          <div className="flex flex-col w-full ">
+            <FormControl>
+              <Input
+                placeholder={placeholder}
+                type={name === "password" ? "password" : "text"}
+                className="input-class"
+                {...field}
+              />
+            </FormControl>
+            <FormMessage className="form-message mt-2" />
+          </div>
+        </div>
+      )}
+    />
+  );
+};
+export default CustomFormInput;
---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://35a29cfd454e2284899fea5e4b8d7b97@o4507345187241984.ingest.de.sentry.io/4507345237966928",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
